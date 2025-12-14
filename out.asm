35e7d454f5c1a2ec791c8f4c22d6ca61b4836829
MartinMayora <martinmayora@gmail.com>
2025-12-14 20:23:28 -0300

    first commit

---
+++ out.asm
@@ -0,0 +1,12 @@
+global _start
+_start:
+    mov rax, 4
+    push rax
+    push QWORD [rsp + 0]
+    push QWORD [rsp + 0]
+    mov rax, 60
+    pop rdi
+    syscall
+    mov rax, 60
+    mov rdi, 0
+    syscall
